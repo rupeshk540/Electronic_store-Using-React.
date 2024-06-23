c2e1dfbb9db0f34b77d08d2dfca3ad2a8190b2a0
rupeshk540 <rrupesh80844@gmail.com>
2024-06-23 11:04:42 +0530

    data binding  and data validation in login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,72 @@
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap"
+import { Alert, Button, Card, Col, Container, Form, Row } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 import Base from "../components/Base"
 import logo from "../assets/logo.png"
+import { useState } from "react"
+import { toast } from "react-toastify"
+import { loginUser } from "../services/user.service"
 
 const Login =()=>{
+
+     let [data,setData]=useState({
+          email:'',
+          password:''
+     })
+    let[error,setError]= useState({
+         errorData:null,
+         isError:false 
+     })
+
+    let [loading,setLoading]= useState(false)
+
+    const handleChange=(event, property) => {
+     setData({
+          ...data,
+          [property]: event.target.value
+     })
+    }
+
+    //submit form
+
+    const submitForm=(event)=>{
+     event.preventDefault();
+
+     console.log(data)
+
+     if(data.email===undefined || data.email.trim()===''){
+          toast.error("Email is required")
+          return;
+     }
+
+     
+     if(data.password===undefined || data.password.trim()===''){
+          toast.error("Password is required")
+          return;
+     }
+
+     //login api
+     setLoading(true)
+     loginUser(data)
+     .then((data)=>{
+          console.log(data)
+          toast.success("logged in")
+          setError({
+               errorData:null,
+               isError:false
+          })
+     })
+     .catch((error)=>{
+          console.log(error)
+          toast.error(error.response.data.message)
+          setError({
+               errorData:error,
+               isError:true
+          })
+     })
+     .finally(()=>{
+          setLoading(false)
+     })
+    }
 
      const loginForm=()=>{
           return(
@@ -22,13 +85,24 @@ const Login =()=>{
 
                                    <h3 className="text-center text-uppercase">Store Login</h3>
 
-                                   <Form>
+                                   <Alert className="mt-2" onClose={()=>setError({
+                                         isError:false,
+                                         errorData:null
+                                   })} dismissible variant="danger" show={error.isError}>
+                                       <Alert.Heading>Hey there ,</Alert.Heading>
+                                       <p> {error.errorData?.response?.data?.message} </p>
+                                   </Alert>
+
+                                   <Form  noValidate onSubmit={submitForm}>
                                         {/* email login field*/}
                                        <Form.Group className="mb-3">
                                           <Form.Label>Enter Email</Form.Label>
                                           <Form.Control
-                                          type="text"
-                                          placeholder="Enter your email"/>
+                                          type="email"
+                                          placeholder="Enter your email"
+                                          onChange={(event)=>handleChange(event,'email')}
+                                          value={data.email}
+                                        />
                                        </Form.Group>
                                         
                                         {/* password login field */}
@@ -36,21 +110,25 @@ const Login =()=>{
                                           <Form.Label>Enter Password</Form.Label>
                                           <Form.Control
                                           type="passwowrd"
-                                          placeholder="Enter your password"/>
+                                          placeholder="Enter your password"
+                                          onChange={(event)=>handleChange(event,'password')}
+                                          value={data.password}/>
                                        </Form.Group>
 
-                                   </Form>
+                                  
                                    
-                                   <Container className="text-center">
-                                   {/* <p>Forgot Password ! <NavLink to="/forgot">Click here</NavLink></p> */}
-                                        <p>If not registered ! <NavLink to="/signup">Click here</NavLink></p>
-                                   </Container>
+                                       <Container className="text-center">
+                                           {/* <p>Forgot Password ! <NavLink to="/forgot">Click here</NavLink></p> */}
+                                           <p>If not registered ! <NavLink to="/signup">Click here</NavLink></p>
+                                        </Container>
 
-                                   {/* button field */}
-                                   <Container className="text-center">
-                                        <Button variant="success">Login</Button>
-                                        <Button className="ms-2" variant="danger">Reset</Button>
-                                   </Container>
+                                           {/* button field */}
+                                        <Container className="text-center">
+                                          <Button type="submit" variant="success">Login</Button>
+                                          <Button className="ms-2" variant="danger">Reset</Button>
+                                       </Container>
+
+                                   </Form>
                               </Card.Body>
 
                          </Card>
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,5 +5,14 @@ import { publicAxios } from "./axios.service";
 
 //register new user
 export const registerUser = (userData)=>{
-    return publicAxios.post(`/users`,userData).then((response)=> response.data);
+    return publicAxios
+    .post(`/users`,userData)
+    .then((response)=> response.data);
+};
+
+//login user
+export const loginUser=(loginData)=>{
+    return publicAxios
+    .post(`/auth/login`,loginData)
+    .then((response)=>response.data)
 };
